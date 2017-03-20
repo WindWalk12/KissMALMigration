6ec69f8de094720686ff2c5c3f5dfcf49c5a3a11
WindWalk12 <nhygta@hotmail.com>
2017-03-20 22:35:12 +0100

    Work with anyone now

--- migration.js
+++ migration.js
@@ -16,6 +16,8 @@ const url2 = /Episode-+[0-9]*\?+id=+[0-9]*$/g;
 
 if (url1.test(url) && url2.test(url) ) {
 
+	var malUser = prompt("Please enter your MAL Username");
+
 	document.getElementById('adsIfrme11').remove();
 	document.getElementById('divFloatRight').remove();
 
@@ -24,6 +26,6 @@ if (url1.test(url) && url2.test(url) ) {
 	$('#container .bigBarContainer #centerDivVideo').after('<div id="mal-iframe" style="display: inline-block; width: 920px; height: 300px; right: 12px; top: 10px; position: relative;"> </div)');
 		console.log("Div works!");
 	
-	$('#mal-iframe').append('<iframe src="https://myanimelist.net/animelist/WindWalk?status=1&tag=" style="display:inline-block; width: 920px; height: 300px"></iframe>');
+	$('#mal-iframe').append('<iframe src="https://myanimelist.net/animelist/'+malUser+'?status=1&tag=" style="display:inline-block; width: 920px; height: 300px"></iframe>');
 		console.log("Iframe works!");
 }
